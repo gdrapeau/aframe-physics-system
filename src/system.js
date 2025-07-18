a3be854fb96d09fac4d34f870c13e68d49bc0ac8
gdrapeau <github@r1d3.com>
2025-07-18 10:46:00 -0400

    Update system.js cannon.es
    removed require for cannon es cause is just use ammo, and three-tocannon reloads three

--- src/system.js
+++ src/system.js
@@ -1,5 +1,5 @@
 /* global THREE */
-var CANNON = require('cannon-es'),
+var CANNON = null, // GD we just want ammo, also thre-tocannon.es reloads three, require('cannon-es'),
     CONSTANTS = require('./constants'),
     C_GRAV = CONSTANTS.GRAVITY,
     C_MAT = CONSTANTS.CONTACT_MATERIAL;
