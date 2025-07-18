e434cb98142b65b2ecc09852258fc8175781e4ff
gdrapeau <github@r1d3.com>
2025-07-18 11:00:53 -0400

    Update system.js patch empty Canon

--- src/system.js
+++ src/system.js
@@ -1,6 +1,6 @@
 /* global THREE */
 console.log("loaded gd patched version of aframe-physics-system")
-var CANNON = null, // GD we just want ammo, also thre-tocannon.es reloads three, require('cannon-es'),
+var CANNON = {}, // GD we just want ammo, also thre-tocannon.es reloads three, require('cannon-es'),
     CONSTANTS = require('./constants'),
     C_GRAV = CONSTANTS.GRAVITY,
     C_MAT = CONSTANTS.CONTACT_MATERIAL;
